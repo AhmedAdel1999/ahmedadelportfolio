3fe79026eeadf49624f57ed4380c6654ede69d38
AhmedAdel1999 <ahmedgithub1999@gmail.com>
2026-07-04 21:34:18 +0300

    change domain

--- src/db.js
+++ src/db.js
@@ -1,150 +1,146 @@
-import img1 from "./img/image-1.png"
-import img2 from "./img/image-2.png"
-import img3 from "./img/image-3.png"
-import img4 from "./img/image-4.png"
-import img5 from "./img/image-5.png"
-import img6 from "./img/image-6.png"
-import img7 from "./img/image-7.png"
-import img8 from "./img/image-8.png"
-import img9 from "./img/image-9.png"
-import img10 from "./img/image-10.png"
-import img11 from "./img/image-11.png"
-import img12 from "./img/image-12.png"
-import img13 from "./img/image-13.png"
-import img14 from "./img/image-14.png"
+import img1 from "./img/image-1.png";
+import img2 from "./img/image-2.png";
+import img3 from "./img/image-3.png";
+import img4 from "./img/image-4.png";
+import img5 from "./img/image-5.png";
+import img6 from "./img/image-6.png";
+import img7 from "./img/image-7.png";
+import img8 from "./img/image-8.png";
+import img9 from "./img/image-9.png";
+import img10 from "./img/image-10.png";
+import img11 from "./img/image-11.png";
+import img12 from "./img/image-12.png";
+import img13 from "./img/image-13.png";
+import img14 from "./img/image-14.png";
 
-const sitesInfo=[
-    {
-        id:14,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://food-deliveryiiapp.netlify.app/',
-        githubUrl:'https://github.com/AhmedAdel1999/food-delivery',
-        imgUrl:img14,
-        siteName:"food delivery"
-    },
-    {
-        id:13,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://admin-dashboardapp.netlify.app/',
-        githubUrl:'https://github.com/AhmedAdel1999/admindashboard',
-        imgUrl:img13,
-        siteName:"admin dashboard"
-    },
-    {
-        id:8,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://tasks-managerapp.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/todaApp',
-        imgUrl:img8,
-        siteName:"Tasks Manager"
-    },
-    {
-        id:7,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://profilesstoreapp.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/profileStoreApp',
-        imgUrl:img7,
-        siteName:"profiles store"
-    },
-    {
-        id:5,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://ecoommerceapp.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/e-commerceApp',
-        imgUrl:img5,
-        siteName:"ecommerce app"
-    },
-    
-    {
-        id:11,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://hotelsystemapp.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/hotel-system',
-        imgUrl:img11,
-        siteName:"hotel system"
-    },
-    {
-        id:12,
-        label:"REACTJS",
-        class:"ALL REACTJS",
-        siteUrl:'https://jobsportalapp.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/jobportal',
-        imgUrl:img12,
-        siteName:"recruitment system"
-    },
-    
-    
-    // {
-    //     id:6,
-    //     label:"REACTJS",
-    //     class:"ALL REACTJS",
-    //     siteUrl:'https://onlinebookslibrary.netlify.app',
-    //     githubUrl:'https://github.com/AhmedAdel1999/onlineLibrary',
-    //     imgUrl:img6,
-    //     siteName:"online library"
-    // },
-    // {
-    //     id:9,
-    //     label:"REACTJS",
-    //     class:"ALL REACTJS",
-    //     siteUrl:'https://blogyyapp.netlify.app',
-    //     githubUrl:'https://github.com/AhmedAdel1999/blogApp',
-    //     imgUrl:img9,
-    //     siteName:"blog app"
-    // },
-    // {
-    //     id:10,
-    //     label:"REACTJS",
-    //     class:"ALL REACTJS",
-    //     siteUrl:'https://facebookeeclonee.netlify.app',
-    //     githubUrl:'https://github.com/AhmedAdel1999/socialMediaApp',
-    //     imgUrl:img10,
-    //     siteName:"facebook clone"
-    // },
-    {
-        id:3,
-        label:"HTML/CSS",
-        class:"ALL HTMLCSS",
-        siteUrl:'https://onnextt.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/onnext',
-        imgUrl:img3,
-        siteName:"on next"
-    },
-    {
-        id:4,
-        label:"HTML/CSS",
-        class:"ALL HTMLCSS",
-        siteUrl:'https://brilliantt.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/brilliantt',
-        imgUrl:img4,
-        siteName:"brilliant"
-    },
-    {
-        id:1,
-        label:"HTML/CSS",
-        class:"ALL HTMLCSS",
-        siteUrl:'https://yamifooood.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/yamifood',
-        imgUrl:img1,
-        siteName:"yamifood"
-    },
-    {
-        id:2,
-        label:"HTML/CSS",
-        class:"ALL HTMLCSS",
-        siteUrl:'https://healthlabb.netlify.app',
-        githubUrl:'https://github.com/AhmedAdel1999/healthlab',
-        imgUrl:img2,
-        siteName:"health lab"
-    },
-    
-    
-    
-]
-export default sitesInfo
+const sitesInfo = [
+  {
+    id: 14,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://food-deliveryiiapp.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/food-delivery",
+    imgUrl: img14,
+    siteName: "food delivery",
+  },
+  {
+    id: 13,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://admin-dashboardapp-five.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/admindashboard",
+    imgUrl: img13,
+    siteName: "admin dashboard",
+  },
+  {
+    id: 8,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://tasks-managerapp.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/todaApp",
+    imgUrl: img8,
+    siteName: "Tasks Manager",
+  },
+  {
+    id: 7,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://profilesstoreapp.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/profileStoreApp",
+    imgUrl: img7,
+    siteName: "profiles store",
+  },
+  {
+    id: 5,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://ecoommerceapp.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/e-commerceApp",
+    imgUrl: img5,
+    siteName: "ecommerce app",
+  },
+
+  {
+    id: 11,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://hotelsystemapp.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/hotel-system",
+    imgUrl: img11,
+    siteName: "hotel system",
+  },
+  {
+    id: 12,
+    label: "REACTJS",
+    class: "ALL REACTJS",
+    siteUrl: "https://jobsportalapp.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/jobportal",
+    imgUrl: img12,
+    siteName: "recruitment system",
+  },
+
+  // {
+  //     id:6,
+  //     label:"REACTJS",
+  //     class:"ALL REACTJS",
+  //     siteUrl:'https://onlinebookslibrary.netlify.app',
+  //     githubUrl:'https://github.com/AhmedAdel1999/onlineLibrary',
+  //     imgUrl:img6,
+  //     siteName:"online library"
+  // },
+  // {
+  //     id:9,
+  //     label:"REACTJS",
+  //     class:"ALL REACTJS",
+  //     siteUrl:'https://blogyyapp.netlify.app',
+  //     githubUrl:'https://github.com/AhmedAdel1999/blogApp',
+  //     imgUrl:img9,
+  //     siteName:"blog app"
+  // },
+  // {
+  //     id:10,
+  //     label:"REACTJS",
+  //     class:"ALL REACTJS",
+  //     siteUrl:'https://facebookeeclonee.netlify.app',
+  //     githubUrl:'https://github.com/AhmedAdel1999/socialMediaApp',
+  //     imgUrl:img10,
+  //     siteName:"facebook clone"
+  // },
+  {
+    id: 3,
+    label: "HTML/CSS",
+    class: "ALL HTMLCSS",
+    siteUrl: "https://onnextt.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/onnext",
+    imgUrl: img3,
+    siteName: "on next",
+  },
+  {
+    id: 4,
+    label: "HTML/CSS",
+    class: "ALL HTMLCSS",
+    siteUrl: "https://brilliant-dusky.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/brilliantt",
+    imgUrl: img4,
+    siteName: "brilliant",
+  },
+  {
+    id: 1,
+    label: "HTML/CSS",
+    class: "ALL HTMLCSS",
+    siteUrl: "https://yamifooood.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/yamifood",
+    imgUrl: img1,
+    siteName: "yamifood",
+  },
+  {
+    id: 2,
+    label: "HTML/CSS",
+    class: "ALL HTMLCSS",
+    siteUrl: "https://healthlabb.vercel.app/",
+    githubUrl: "https://github.com/AhmedAdel1999/healthlab",
+    imgUrl: img2,
+    siteName: "health lab",
+  },
+];
+export default sitesInfo;
